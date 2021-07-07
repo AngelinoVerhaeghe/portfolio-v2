ed61f34302392d99e19d2de4c658998e8224daf1
Sonicastra <Sonicastra@telenet.be>
2021-07-07 09:49:19 +0200

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,190 @@ function toggleSkills() {
 skillsHeader.forEach((element) => {
   element.addEventListener("click", toggleSkills);
 });
+
+/* Qualification Tabs */
+
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+
+    tab.classList.add("qualification__active");
+  });
+});
+
+/* SERVICES MODAL */
+
+/* Grab the items in the DOM */
+
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
+
+/* PORTFOLIO SWIPER */
+
+let swiperPortfolio = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true, // Option
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // Option
+  },
+  /* mousewheel: true,
+  keyboard: true, */
+});
+
+/* SWIPER STYLING PAGINATION DYNAMIC */
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  //cssMode: true,
+  loop: true, // Option
+  grabCursor: true, // Option
+  spaceBetween: 48, // Option
+  /* navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }, */
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // Option
+    dynamicBullets: true, // Option
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+  /* mousewheel: true,
+  keyboard: true, */
+});
+
+/* SCROLL SECTIONS ACTIVE LINK */
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+/* CHANGE BACKGROUND HEADER JS */
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+
+  // When the scroll is greater then 80 viewport height, add the scroll-header to the header tag
+  if (this.scrollY >= 80) {
+    nav.classList.add("scroll-header");
+  } else {
+    nav.classList.remove("scroll-header");
+  }
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+/* SHOW SCROLL BUTTON TO TOP */
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+
+  // When the scroll is greater then 560 viewport height, add the scroll-header to the header tag
+  if (this.scrollY >= 560) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+
+window.addEventListener("scroll", scrollUp);
+
+/* DARK / LIGHT THEME JS */
+
+const themeButton = document.getElementById("theme-button");
+
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun"; // This is the class if icon u choose, from Fontawesome, Remixicon etc...
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+// We validate if user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark theme
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / Deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or Remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  // We save the theme and the current icon that the user choose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
